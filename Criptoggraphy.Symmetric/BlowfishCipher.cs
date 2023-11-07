74e7de9272e2843a216a15400cf3f2f88cfbed57
Martinho Sebastião <martinho.sebastiao@outlook.com>
2023-11-07 19:01:15 +0100

    Uso da cifra de Blowfish
    Reajuste do program para atender os testes da utilização da cifra Blowfish

---
+++ Criptoggraphy.Symmetric/BlowfishCipher.cs
@@ -0,0 +1,36 @@
+﻿using Blowfish = BCrypt.Net.BCrypt;
+
+namespace Criptography.Symmetric
+{
+    public sealed class BlowfishCipher
+    {
+        public static string Encrypt(string plainText)
+        {
+            try
+            {
+                var hash = Blowfish.HashPassword(plainText);
+
+                return hash;
+            }
+            catch (Exception ex)
+            {
+                return string.Concat("Erro: ", ex.Message);
+            }
+        }
+
+        public static bool Verify(string plainText, string cipherText)
+        {
+            try
+            {
+                var result = Blowfish.Verify(plainText, cipherText);
+
+                return result;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
+
--- Criptoggraphy.Symmetric/Program.cs
+++ Criptoggraphy.Symmetric/Program.cs
@@ -4,18 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Criptografia usando Algoritmo AES!\n");
+        Console.WriteLine("Criptografia usando Algoritmo Blowfish!\n");
 
-        // Chave a ser usada para encriptar e desencriptar.
-        const string key = "b14ca5898a4e4133bbce2ea2315a1916";
+      
         const string plainText = ".NET Core é vida!";
 
-        var cipherText = AlgorithmAES.Encrypt(plainText, key);
-        var decryptedText = AlgorithmAES.Decrypt(cipherText, key);
+        var cipherText = BlowfishCipher.Encrypt(plainText);
+        var result = BlowfishCipher.Verify(plainText,cipherText);
 
         Console.WriteLine("Texto Legivel ==> {0}\n",plainText);
-        Console.WriteLine("Encriptado ==> {0}\n", cipherText);
-        Console.WriteLine("Desencriptado ==> {0}\n", decryptedText);
+        Console.WriteLine("Transformado em Hash ==> {0}\n", cipherText);
+        Console.WriteLine("A Hash é valida? ==> {0}\n", result);
 
         Console.ReadKey();
 
